a352ac52173d1bd7979e61e4bc8f96be03e3a567
William <William>
2018-08-01 16:14:18 -0700

    Add mutation

--- GraphQLTutorial/Application/Mutation/Resolvers/HelloWorld3Resolver.cs
+++ GraphQLTutorial/Application/Mutation/Resolvers/HelloWorld3Resolver.cs
@@ -11,9 +11,56 @@ namespace GraphQLTutorial.Application.Mutation.Resolvers
         public void Resolve(GraphQLMutation graphQLMutation)
         {
             graphQLMutation.Field<StringGraphType>(
-                name: "hello3",
+                name: "create",
                 resolve: context => "Changing"
             );
+
+            /*
+                {
+	                "query" : "mutation ($item:ItemInput!) {createName(item:$item) { name}	}",
+	                "variables": {
+	                 "item": {
+	                   "name": "Test"
+	                 }
+	                }
+                }
+             */
+            graphQLMutation.Field<ItemType>(
+            //graphQLMutation.Field<StringGraphType>(
+                "createName",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<ItemInputType>> { Name = "item" }
+                ),
+                resolve: context =>
+                {
+                    var item = context.GetArgument<Item>("item");
+                    return new Item() { Name = " Changed " };
+                });
+
+        }
+    }
+
+    public class ItemInputType : InputObjectGraphType
+    {
+        public ItemInputType()
+        {
+            Name = "ItemInput";
+            Field<NonNullGraphType<StringGraphType>>("name");
+        }
+    }
+
+    public class Item
+    {
+
+        public string Name { get; set; }
+
+    }
+
+    public class ItemType : ObjectGraphType<Item>
+    {
+        public ItemType()
+        {
+            Field(i => i.Name);
         }
     }
 }
