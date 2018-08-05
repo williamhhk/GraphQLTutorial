ce3916f4b717faf37efe9d7427394877a481e001
William <William>
2018-08-05 14:37:14 -0700

    Add Powershell test script

---
+++ GraphQLTutorial/WebCall.ps1
@@ -0,0 +1,15 @@
+﻿$url = "http://localhost:58001/api/graphql"
+$input = "Test"
+
+$json = @{
+    query = "mutation (`$item:ItemInput!) {createName(item:`$item){name}}"
+    variables = @{"item" = @{ "name" = $input}}
+    }
+$body = $json | ConvertTo-Json
+$body
+$headers = @{
+    "Content-Type" = "application/json"
+}
+
+$response = Invoke-WebRequest -Uri $url -Method POST -Body $body -Headers $headers | ConvertFrom-Json
+$response.data.createName
